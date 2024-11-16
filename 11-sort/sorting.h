32bbcae6b0f0667afa7b5be72e69bc95d7ccfe29
GhostcCoder <116057784+GhostcCoder@users.noreply.github.com>
2024-11-17 00:16:09 +0900

    Create sorting.h

---
+++ 11-sort/sorting.h
@@ -0,0 +1,5 @@
+extern void selection_sort(int list[], int n);
+extern void insertion_sort(int list[], int n);
+extern void bubble_sort(int list[], int n);
+extern void shell_sort(int list[], int n);
+extern void print_list(int list[], int n);
