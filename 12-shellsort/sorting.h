2ea00a74920d29704e1d944091b04151f0620471
GhostcCoder <116057784+GhostcCoder@users.noreply.github.com>
2024-11-17 16:28:34 +0900

    Create sorting.h

---
+++ 12-shellsort/sorting.h
@@ -0,0 +1,5 @@
+extern int gap_insertion_sort(int list[], int first, int last, int gap, int *moveCount);
+extern void doShellSort(int list[], int n, int *comparisonCount, int *moveCount);
+extern void print_list(int list[], int n);
+extern void generateRandomNumbers(int array[]);
+extern void doInsertionSort(int list[], int n, int *comparisonCount, int *moveCount);
