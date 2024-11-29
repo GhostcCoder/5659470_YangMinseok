3d000ac6229f7d7856f6c67793868207468cdd85
GhostcCoder <116057784+GhostcCoder@users.noreply.github.com>
2024-11-29 23:26:24 +0900

    Update merge.h

--- 13-mergesort/merge.h
+++ 13-mergesort/merge.h
@@ -1,5 +1,5 @@
 void generateRandomArray(int array[]);
 void printArray(int array[], int n);
 int getMergeSortCompareCount(int array[], int *moveCount);
-void mergeSort(int array[], int left, int right);
+void doMergeSort(int array[], int left, int right);
 void merge(int array[], int left, int mid, int right, int *compareCount, int *moveCount);
